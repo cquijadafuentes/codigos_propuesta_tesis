bd2795aec0c305b5f7ed8c314222ea577c55118d
Carlos <cquijadafuentes@gmail.com>
2022-10-28 12:21:31 -0300

    Limpieza de archivos.

--- codes_sdsl_cst/TopoRel_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "*********** GST ***********\n"
-./TopoRel_GST_test < ./TopoRel_test_input.txt
-echo "********** Naive **********\n"
-./TopoRel_Naive_test < ./TopoRel_test_input.txt
--- codes_sdsl_cst/TopoRel_valgrind_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-make
-valgrind --leak-check=yes ./TopoRel_Naive_test < TopoRel_test_input.txt
-valgrind --leak-check=yes ./TopoRel_GST_test < TopoRel_test_input.txt
