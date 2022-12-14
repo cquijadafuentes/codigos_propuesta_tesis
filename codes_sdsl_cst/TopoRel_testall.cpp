b7d591c265f8334e4234b49378d32a7b9a977181
Carlos <cquijadafuentes@gmail.com>
2022-12-14 03:18:32 -0300

    Actualización de test.

--- codes_sdsl_cst/TopoRel_testall.cpp
+++ codes_sdsl_cst/TopoRel_testall.cpp
@@ -9,11 +9,12 @@
 #include "TopoRel_GST_0.hpp"
 #include "TopoRel_GST_1.hpp"
 #include "TopoRel_GST_2.hpp"
+#include "TopoRel_GST_4.hpp"
 
 using namespace std;
 
-bool noIguales(bool a, bool b, bool c, bool d, bool e){
-	if(a != b || b != c || c != d || d != e){
+bool noIguales(bool a, bool b, bool c, bool d, bool e, bool f){
+	if(a != b || b != c || c != d || d != e || e != f){
 		return true;
 	}
 	return false;
@@ -35,11 +36,12 @@ int main(int argc, char const *argv[]){
     TopoRelGST_0 tt_0(routes, n_stops);
     TopoRelGST_1 tt_1(routes, n_stops);
     TopoRelGST_2 tt_2(routes, n_stops);
+    TopoRelGST_4 tt_4(routes, n_stops);
     TopoRelNaivePreComp trpc(routes, n_stops);
     int contAgregadas = 0;
     int contBasicas = 0;
 
-    string rNaive, rNaivePC, rGST_0, rGST_1, rGST_2;
+    string rNaive, rNaivePC, rGST_0, rGST_1, rGST_2, rGST_4;
     for(int i = 0; i < routes.size(); i++){
         for(int j = 0; j < routes.size(); j++){
             rNaive = toporel(routes[i], routes[j]);
@@ -47,14 +49,16 @@ int main(int argc, char const *argv[]){
             rGST_0 = tt_0.obtenerRelacion(i, j);
             rGST_1 = tt_1.obtenerRelacion(i, j);
             rGST_2 = tt_2.obtenerRelacion(i, j);
+            rGST_4 = tt_4.obtenerRelacion(i, j);
             // 8 Relaciones básicas 
-            if(rNaive != rNaivePC || rNaivePC != rGST_0 || rGST_0 != rGST_1 || rGST_1 != rGST_2){
+            if(rNaive != rNaivePC || rNaivePC != rGST_0 || rGST_0 != rGST_1 || rGST_1 != rGST_2 || rGST_2 != rGST_4){
             	cout << "i: " << i << " - j: " << j << endl;
             	cout << "\trNaive: " << rNaive << endl;
             	cout << "\trNaivePC: " << rNaivePC << endl;
             	cout << "\trGST_0: " << rGST_0 << endl;
             	cout << "\trGST_1: " << rGST_1 << endl;
             	cout << "\trGST_2: " << rGST_2 << endl;
+            	cout << "\trGST_4: " << rGST_4 << endl;
 	            cout << "\t(" << routes[i].size() << "): ";
 	            for(int x=0; x<routes[i].size(); x++){
 	            	cout << routes[i][x] << " ";
@@ -75,24 +79,27 @@ int main(int argc, char const *argv[]){
 			bool tt0With = tt_0.tr_within(i,j);
 			bool tt1With = tt_1.tr_within(i,j);
 			bool tt2With = tt_2.tr_within(i,j);
+			bool tt4With = tt_4.tr_within(i,j);
 			bool trCont = tr_contains(routes[i], routes[j]);
 			bool trpcCont = trpc.contains(i, j);
 			bool tt0Cont = tt_0.tr_contains(i, j);
 			bool tt1Cont = tt_1.tr_contains(i, j);
 			bool tt2Cont = tt_2.tr_contains(i, j);
+			bool tt4Cont = tt_4.tr_contains(i, j);
 			bool trInte = tr_intersect(routes[i], routes[j]);
 			bool trpcInte = trpc.intersect(i, j);
 			bool tt0Inte = tt_0.tr_intersect(i, j);
 			bool tt1Inte = tt_1.tr_intersect(i, j);
 			bool tt2Inte = tt_2.tr_intersect(i, j);
+			bool tt4Inte = tt_4.tr_intersect(i, j);
 
-            if(noIguales(trWith, trpcWith, tt0With, tt1With, tt2With) ||
-            		noIguales(trCont, trpcCont, tt0Cont, tt1Cont, tt2Cont) ||
-            		noIguales(trInte, trpcInte, tt0Inte, tt1Inte, tt2Inte)){
-            	cout << "\tNaive\tNaivePC\tGST_0\tGST_1\tGST_2" << endl;
-            	cout << "With\t" << trWith << "\t" << trpcWith << "\t" << tt0With << "\t" << tt1With << "\t" << tt2With << endl;
-            	cout << "Cont\t" << trCont << "\t" << trpcCont << "\t" << tt0Cont << "\t" << tt1Cont << "\t" << tt2Cont << endl;
-            	cout << "Inte\t" << trInte << "\t" << trpcInte << "\t" << tt0Inte << "\t" << tt1Inte << "\t" << tt2Inte << endl;
+            if(noIguales(trWith, trpcWith, tt0With, tt1With, tt2With, tt4With) ||
+            		noIguales(trCont, trpcCont, tt0Cont, tt1Cont, tt2Cont, tt4Cont) ||
+            		noIguales(trInte, trpcInte, tt0Inte, tt1Inte, tt2Inte, tt4Inte)){
+            	cout << "\tNaive\tNaivePC\tGST_0\tGST_1\tGST_2\tGST_4" << endl;
+            	cout << "With\t" << trWith << "\t" << trpcWith << "\t" << tt0With << "\t" << tt1With << "\t" << tt2With << "\t" << tt4With << endl;
+            	cout << "Cont\t" << trCont << "\t" << trpcCont << "\t" << tt0Cont << "\t" << tt1Cont << "\t" << tt2Cont << "\t" << tt4Cont << endl;
+            	cout << "Inte\t" << trInte << "\t" << trpcInte << "\t" << tt0Inte << "\t" << tt1Inte << "\t" << tt2Inte << "\t" << tt4Inte << endl;
             	cout << "Rel\t" << rNaive << "\t" << rNaivePC << "\t" << rGST_0 << "\t" << rGST_1 << "\t" << rGST_2 << endl;
             	cout << "\t(" << routes[i].size() << "): ";
 	            for(int x=0; x<routes[i].size(); x++){
