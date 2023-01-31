fe3767249c68cf41bbc0035ced6bfa1b90520b18
Carlos <cquijadafuentes@gmail.com>
2023-01-31 00:48:45 -0300

    actualización de run constructores para udec_server.

--- codes_sdsl_cst/run_test_comparaGST4constructores.sh
+++ codes_sdsl_cst/run_test_comparaGST4constructores.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "***************** Madrid *****************" > ~/GST/resultados_2023-01-31_constructores.txt
-~/GST/codigos_propuesta_tesis/codes_sdsl_cst/test_comparaGST4constructores < ~/GST/datasets/madrid.txt >> ~/GST/resultados_2023-01-31_constructores.txt
+~/GST/codigos_propuesta_tesis/codes_sdsl_cst/test_comparaGST4constructores < ~/GST/datasets/madrid_x1 >> ~/GST/resultados_2023-01-31_constructores.txt
 echo "***************** Madrid_x2 *****************" >> ~/GST/resultados_2023-01-31_constructores.txt
 ~/GST/codigos_propuesta_tesis/codes_sdsl_cst/test_comparaGST4constructores < ~/GST/datasets/madrid_x2 >> ~/GST/resultados_2023-01-31_constructores.txt
 echo "***************** Madrid_x4 *****************" >> ~/GST/resultados_2023-01-31_constructores.txt
