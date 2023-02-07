f157a8690b691a4d9ebd541bca8b3b77618bd04f
CQF <cquijadafuentes@gmail.com>
2023-02-06 22:04:52 -0300

    actualiza script lote gst5

--- codes_sdsl_cst/run_time_allContain_GST5.sh
+++ codes_sdsl_cst/run_time_allContain_GST5.sh
@@ -10,12 +10,12 @@ DATAFOLDER=$1
 OUTPUTFILE=$2
 
 
-echo "Fecha inicio experimentos: $(date +'%Y/%m/%d %H:%M:%S')" > ${OUTPUTFILE}
+echo "Fecha INICIO experimentos: $(date +'%Y/%m/%d %H:%M:%S')" > ${OUTPUTFILE}
 
 for filename in gst_tripsMadrid_0100k gst_tripsMadrid_0200k gst_tripsMadrid_0400k gst_tripsMadrid_0800k gst_tripsMadrid_1600k gst_tripsMadrid_3200k
 do
 	./TopoRel_GST_5_save ${DATAFOLDER}${filename}.gst5 < ${DATAFOLDER}${filename}.txt >> ${OUTPUTFILE}
 	./time_TopoRel_allContain_GST5_xLote ${DATAFOLDER}${filename}.gst5 10000 5 < ${DATAFOLDER}${filename}.txt >> ${OUTPUTFILE}
 done
-echo "Fecha fin experimentos: $(date +'%Y/%m/%d %H:%M:%S')" >> ${OUTPUTFILE}
+echo "Fecha FIN experimentos: $(date +'%Y/%m/%d %H:%M:%S')" >> ${OUTPUTFILE}
 exit 0
