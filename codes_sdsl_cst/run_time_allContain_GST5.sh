d94118539f7e42ac4115884b5e9a714e134840ed
CQF <cquijadafuentes@gmail.com>
2023-02-07 17:15:43 -0300

    actualización para correr time_allContains en compostela.

--- codes_sdsl_cst/run_time_allContain_GST5.sh
+++ codes_sdsl_cst/run_time_allContain_GST5.sh
@@ -15,7 +15,7 @@ echo "Fecha INICIO experimentos: $(date +'%Y/%m/%d %H:%M:%S')" > ${OUTPUTFILE}
 for filename in gst_tripsMadrid_0005k gst_tripsMadrid_0010k gst_tripsMadrid_0015k gst_tripsMadrid_0020k gst_tripsMadrid_0025k gst_tripsMadrid_0030k gst_tripsMadrid_0035k gst_tripsMadrid_0040k gst_tripsMadrid_0045k gst_tripsMadrid_0050k
 do
 	./TopoRel_GST_5_save ${DATAFOLDER}${filename}.gst5 < ${DATAFOLDER}${filename}.txt
-	./time_TopoRel_allContain_GST5_xLote ${DATAFOLDER}${filename}.gst5 10000 5 < ${DATAFOLDER}${filename}.txt >> ${OUTPUTFILE}
+	./time_TopoRel_allContain_GST5_xLote ${DATAFOLDER}${filename}.gst5 1000 5 < ${DATAFOLDER}${filename}.txt >> ${OUTPUTFILE}
 done
 echo "Fecha FIN experimentos: $(date +'%Y/%m/%d %H:%M:%S')" >> ${OUTPUTFILE}
 exit 0
