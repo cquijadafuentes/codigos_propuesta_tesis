7703544797379879adcfef824c38c5d108051a01
CQF <cquijadafuentes@gmail.com>
2023-02-06 22:00:04 -0300

    actualiza script lote gst5

--- codes_sdsl_cst/run_time_allContain_GST5.sh
+++ codes_sdsl_cst/run_time_allContain_GST5.sh
@@ -19,32 +19,3 @@ do
 done
 echo "Fecha fin experimentos: $(date +'%Y/%m/%d %H:%M:%S')" >> ${OUTPUTFILE}
 exit 0
-
-
-echo "Ejecución para 10000 consultas..."
-././time_TopoRel_allContain_GST5_xLote ${INPUTGST5} 10000 < ${INPUTTXT} >> ${OUTPUTFILE}
-echo "Ejecución para 20000 consultas..."
-./time_TopoRel_all_contain_GST5 ${INPUTGST5} 20000 < ${INPUTTXT} >> ${OUTPUTFILE}
-echo "Ejecución para 30000 consultas..."
-./time_TopoRel_all_contain_GST5 ${INPUTGST5} 30000 < ${INPUTTXT} >> ${OUTPUTFILE}
-echo "Ejecución para 40000 consultas..."
-./time_TopoRel_all_contain_GST5 ${INPUTGST5} 40000 < ${INPUTTXT} >> ${OUTPUTFILE}
-echo "Ejecución para 50000 consultas..."
-./time_TopoRel_all_contain_GST5 ${INPUTGST5} 50000 < ${INPUTTXT} >> ${OUTPUTFILE}
-echo "Ejecución para 60000 consultas..."
-./time_TopoRel_all_contain_GST5 ${INPUTGST5} 60000 < ${INPUTTXT} >> ${OUTPUTFILE}
-echo "Ejecución para 70000 consultas..."
-./time_TopoRel_all_contain_GST5 ${INPUTGST5} 70000 < ${INPUTTXT} >> ${OUTPUTFILE}
-echo "Ejecución para 80000 consultas..."
-./time_TopoRel_all_contain_GST5 ${INPUTGST5} 80000 < ${INPUTTXT} >> ${OUTPUTFILE}
-echo "Ejecución para 90000 consultas..."
-./time_TopoRel_all_contain_GST5 ${INPUTGST5} 90000 < ${INPUTTXT} >> ${OUTPUTFILE}
-echo "Ejecución para 100000 consultas..."
-./time_TopoRel_all_contain_GST5 ${INPUTGST5} 100000 < ${INPUTTXT} >> ${OUTPUTFILE}
-
-gst_tripsMadrid_0100k.txt
-gst_tripsMadrid_0200k.txt
-gst_tripsMadrid_0400k.txt
-gst_tripsMadrid_0800k.txt
-gst_tripsMadrid_1600k.txt
-gst_tripsMadrid_3200k.txt
