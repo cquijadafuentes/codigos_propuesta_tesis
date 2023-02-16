aa0dd63ed896f965f93082914ff12d8a19a10701
CQF <cquijadafuentes@gmail.com>
2023-02-16 11:52:18 -0300

    script para tomar size.

---
+++ codes_sdsl_cst/run_sizeToPlot_gst5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "$(basename $0) <data_folder> <output_file>"
+	echo "Generará resultados para las 3 operaciones de conjunto con 2000 consultas y 10 repeticiones."
+	exit 1
+fi
+
+DATAFOLDER=$1
+OUTPUTFILE=$2
+
+
+echo "Fecha INICIO experimentos: $(date +'%Y/%m/%d %H:%M:%S')" > ${OUTPUTFILE}
+
+for filename in gst_tripsMadrid_0005k gst_tripsMadrid_0010k gst_tripsMadrid_0015k gst_tripsMadrid_0020k gst_tripsMadrid_0025k gst_tripsMadrid_0030k gst_tripsMadrid_0035k gst_tripsMadrid_0040k gst_tripsMadrid_0045k gst_tripsMadrid_0050k
+do
+	./TopoRel_GST_5_size ${DATAFOLDER}${filename}.gst5 >> ${OUTPUTFILE}
+done
+echo "Fecha FIN experimentos: $(date +'%Y/%m/%d %H:%M:%S')" >> ${OUTPUTFILE}
+exit 0
