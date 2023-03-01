1ced999dcacb66cc3be8d102c045dbac42803cb0
Carlos <cquijadafuentes@gmail.com>
2023-03-01 02:19:19 -0300

    script para automatizar la contibilización de celdas del lcp que se recorren durante la operacón allcontained.

--- codes_sdsl_cst/run_howManyLCP_groupOperations.sh
+++ codes_sdsl_cst/run_howManyLCP_groupOperations.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 2 ]; then
 	echo "$(basename $0) <data_folder> <output_file>"
-	echo "Generará resultados operación de allContained con 2000 consultas y 1 repetición y promedio de celdas recorridas del LCP durante la operación."
+	echo "Generará resultados operación de allContained con 500 consultas y 1 repetición y promedio de celdas recorridas del LCP durante la operación."
 	exit 1
 fi
 
@@ -14,7 +14,7 @@ echo "Fecha INICIO experimentos: $(date +'%Y/%m/%d %H:%M:%S')" > ${OUTPUTFILE}
 
 for filename in gst_tripsMadrid_0005k gst_tripsMadrid_0010k gst_tripsMadrid_0015k gst_tripsMadrid_0020k gst_tripsMadrid_0025k gst_tripsMadrid_0030k gst_tripsMadrid_0035k gst_tripsMadrid_0040k gst_tripsMadrid_0045k gst_tripsMadrid_0050k
 do
-	./test_TopoRel_all_contained_howManyLCP ${DATAFOLDER}${filename}.gst5 2000 10 >> ${OUTPUTFILE}
+	./test_TopoRel_all_contained_howManyLCP ${DATAFOLDER}${filename}.gst5 500 10 >> ${OUTPUTFILE}
 done
 echo "Fecha FIN experimentos: $(date +'%Y/%m/%d %H:%M:%S')" >> ${OUTPUTFILE}
 exit 0
