b47ac3c698e969a393622a8e8cf1d6a895753ee4
Carlos <cquijadafuentes@gmail.com>
2023-03-02 11:44:32 -0300

    mejora en el script de experimentos.

--- codes_sdsl_cst/run_howManyLCP_groupOperations.sh
+++ codes_sdsl_cst/run_howManyLCP_groupOperations.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "$(basename $0) <data_folder> <output_file>"
+if [ $# -ne 4 ]; then
+	echo "$(basename $0) <data_folder> <output_file> consultas repeticiones"
 	echo "Generará resultados operación de allContained con 500 consultas y 1 repetición y promedio de celdas recorridas del LCP durante la operación."
 	exit 1
 fi
 
 DATAFOLDER=$1
 OUTPUTFILE=$2
+CONSULTAS=$3
+REPETICIONES=$4
 
 
 echo "Fecha INICIO experimentos: $(date +'%Y/%m/%d %H:%M:%S')" > ${OUTPUTFILE}
 
 for filename in gst_tripsMadrid_0005k gst_tripsMadrid_0010k gst_tripsMadrid_0015k gst_tripsMadrid_0020k gst_tripsMadrid_0025k gst_tripsMadrid_0030k gst_tripsMadrid_0035k gst_tripsMadrid_0040k gst_tripsMadrid_0045k gst_tripsMadrid_0050k
 do
-	./test_TopoRel_all_contained_howManyLCP ${DATAFOLDER}${filename}.gst5 500 1 >> ${OUTPUTFILE}
+	./test_TopoRel_all_contained_howManyLCP ${DATAFOLDER}${filename}.gst5 ${CONSULTAS} ${REPETICIONES} >> ${OUTPUTFILE}
 done
 echo "Fecha FIN experimentos: $(date +'%Y/%m/%d %H:%M:%S')" >> ${OUTPUTFILE}
 exit 0
