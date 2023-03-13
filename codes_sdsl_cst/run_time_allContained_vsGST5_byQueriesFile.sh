50f49d103ac295fa104b030ef73986a89f09dc8b
CQF <cquijadafuentes@gmail.com>
2023-03-13 19:57:38 -0300

    update de run_time_byqueries.

--- codes_sdsl_cst/run_time_allContained_vsGST5_byQueriesFile.sh
+++ codes_sdsl_cst/run_time_allContained_vsGST5_byQueriesFile.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
-	echo "$(basename $0) <data_folder_1> <data_folder_2> <output_file> repeticiones"
+if [ $# -ne 5 ]; then
+	echo "$(basename $0) <data_folder_1> <data_folder_2> <output_file> <repeticiones1> <repeticiones2>"
 	echo "Generará resultados para las 3 operaciones de conjunto <repeticiones> veces con los archivos de consulta correspondientes."
+	echo "<repeticiones1> es para los experimentos de queries.1 y <repeticiones2> es para queries.2 y queries.3"
 	exit 1
 fi
 
 DATAFOLDER1=$1
 DATAFOLDER2=$2
 OUTPUTFILE=$3
-REPETICIONES=$4
+REPETICIONES1=$4
+REPETICIONES2=$5
 
 
 echo "Fecha INICIO experimentos: $(date +'%Y/%m/%d %H:%M:%S')" > ${OUTPUTFILE}
@@ -18,11 +20,17 @@ echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> ${OUTPUTFI
 echo "**************** DATASET = ${DATAFOLDER1} ****************" >> ${OUTPUTFILE}
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> ${OUTPUTFILE}
 
-for i in 1 2 3 
+for filename in gst_tripsMadrid_0005k gst_tripsMadrid_0010k gst_tripsMadrid_0015k gst_tripsMadrid_0020k gst_tripsMadrid_0025k
+do
+	./time_TopoRel_allContained_vsGST5_byQueriesFile ${DATAFOLDER1}${filename}.gst5 ${DATAFOLDER1}${filename}.queries.1 ${REPETICIONES1} >> ${OUTPUTFILE}
+done
+echo " ----------------------------------------------------- " >> ${OUTPUTFILE}
+
+for i in 2 3 
 do
 	for filename in gst_tripsMadrid_0005k gst_tripsMadrid_0010k gst_tripsMadrid_0015k gst_tripsMadrid_0020k gst_tripsMadrid_0025k
 	do
-		./time_TopoRel_allContained_vsGST5_byQueriesFile ${DATAFOLDER1}${filename}.gst5 ${DATAFOLDER1}${filename}.queries.${i} ${REPETICIONES} >> ${OUTPUTFILE}
+		./time_TopoRel_allContained_vsGST5_byQueriesFile ${DATAFOLDER1}${filename}.gst5 ${DATAFOLDER1}${filename}.queries.${i} ${REPETICIONES2} >> ${OUTPUTFILE}
 	done
 	echo " ----------------------------------------------------- " >> ${OUTPUTFILE}
 done
@@ -31,11 +39,19 @@ echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> ${OUTPUTFI
 echo "**************** DATASET = ${DATAFOLDER2} ****************" >> ${OUTPUTFILE}
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" >> ${OUTPUTFILE}
 
-for i in 1 2 3 
+for filename in 20stops_min_05k  20stops_min_10k  20stops_min_15k  20stops_min_20k  20stops_min_25k
+do
+	./time_TopoRel_allContained_vsGST5_byQueriesFile ${DATAFOLDER2}${filename}.gst5 ${DATAFOLDER2}${filename}.queries.1 ${REPETICIONES1} >> ${OUTPUTFILE}
+done
+echo " ----------------------------------------------------- " >> ${OUTPUTFILE}
+
+
+
+for i in 2 3 
 do
 	for filename in 20stops_min_05k  20stops_min_10k  20stops_min_15k  20stops_min_20k  20stops_min_25k
 	do
-		./time_TopoRel_allContained_vsGST5_byQueriesFile ${DATAFOLDER2}${filename}.gst5 ${DATAFOLDER2}${filename}.queries.${i} ${REPETICIONES} >> ${OUTPUTFILE}
+		./time_TopoRel_allContained_vsGST5_byQueriesFile ${DATAFOLDER2}${filename}.gst5 ${DATAFOLDER2}${filename}.queries.${i} ${REPETICIONES2} >> ${OUTPUTFILE}
 	done
 	echo " ----------------------------------------------------- " >> ${OUTPUTFILE}
 done
