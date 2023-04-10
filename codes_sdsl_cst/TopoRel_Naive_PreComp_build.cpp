6183f768fdaaa49e10f34ae09ead77fc2a02249a
CQF <cquijadafuentes@gmail.com>
2023-04-10 09:35:55 -0400

    updates en construcciones.

--- codes_sdsl_cst/TopoRel_Naive_PreComp_build.cpp
+++ codes_sdsl_cst/TopoRel_Naive_PreComp_build.cpp
@@ -6,27 +6,32 @@ using namespace sdsl;
 
 int main(int argc, char const *argv[]){
 
-	if(argc < 2){
+	if(argc < 3){
 		cout << "Error! faltan argumentos." << endl;
-		cout << argv[0] << " <output_filename>" << endl;
+		cout << argv[0] << "<input_filename.txt> <output_filename.gst6>" << endl;
+		cout << "Input_File format:" << endl;
+		cout << "cant_rutas max_stop" << endl;
+		cout << "cant_stops_r1 r1_st1 r1_st2 ···" << endl;
+		cout << "cant_stops_r2 r2_st1 r2_st2 ···" << endl;
+		cout << "···" << endl << endl;
 		return 0;
 	}
-	cout << "Input:" << endl;
-	cout << "cant_rutas max_stop" << endl;
-	cout << "cant_stops_r1 r1_st1 r1_st2 ···" << endl;
-	cout << "cant_stops_r2 r2_st1 r2_st2 ···" << endl;
-	cout << "···" << endl << endl;
+
+
+	ifstream txtInput(argv[1], ifstream::in);
 
 	int n, x, aux, max;
-	cin >> n >> max;
+	txtInput >> n >> max;
 	vector<vector<int>> lx(n);
 	for(int i = 0; i < n; i++){
-		cin >> x;
+		txtInput >> x;
 		for(int j=0; j<x; j++){
-			cin >> aux;
+			txtInput >> aux;
 			lx[i].push_back(aux);
 		}
 	}
+	txtInput.close();
+
 	cout << lx.size() << endl;
 	for(int i=0; i<lx.size(); i++){
 		cout << lx[i].size() << ": ";
@@ -39,7 +44,7 @@ int main(int argc, char const *argv[]){
 	
 	TopoRelNaivePreComp tpnPC_1(lx, max);
 
-	string filename(argv[1]);
+	string filename(argv[2]);
 	if(tpnPC_1.save(filename)){
 		cout << "Se ha guardado exitosamente." << endl;
 	}else{
