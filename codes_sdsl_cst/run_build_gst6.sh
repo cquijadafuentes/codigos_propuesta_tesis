55cae2c78cec8e1a99976b3e3d56e211111b9197
CQF <cquijadafuentes@gmail.com>
2023-04-18 10:37:01 -0400

    updates.

--- codes_sdsl_cst/run_build_gst6.sh
+++ codes_sdsl_cst/run_build_gst6.sh
@@ -11,5 +11,5 @@ DATAFOLDER=$1
 for filename in 20stops_min_05k 20stops_min_10k 20stops_min_15k 20stops_min_20k 20stops_min_25k 
 do
 	./TopoRel_GST_6_build ${DATAFOLDER}${filename}.txt ${DATAFOLDER}${filename}.gst6
-	./TopoRel_Naive_PreComp_build ${DATAFOLDER}${filename}.txt ${DATAFOLDER}${filename}.naivepc
+	#./TopoRel_Naive_PreComp_build ${DATAFOLDER}${filename}.txt ${DATAFOLDER}${filename}.naivepc
 done
