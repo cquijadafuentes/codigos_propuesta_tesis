daf05bf9bec2941677fc7d0865e7d3ca58a6137c
CQF <cquijadafuentes@gmail.com>
2023-04-20 10:57:00 -0400

    script de size para gst6 y plots.

--- codes_sdsl_cst/run_sizeToPlot_gst6.sh
+++ codes_sdsl_cst/run_sizeToPlot_gst6.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 2 ]; then
 	echo "$(basename $0) <data_folder> <output_file>"
-	echo "Generará archivo con nombre <output_file> que contiene los tamaños de las estructuras que forman gst5."
+	echo "Generará archivo con nombre <output_file> que contiene los tamaños de las estructuras que forman gst6."
 	exit 1
 fi
 
@@ -14,7 +14,7 @@ echo "Fecha INICIO experimentos: $(date +'%Y/%m/%d %H:%M:%S')" > ${OUTPUTFILE}
 
 for filename in gst_tripsMadrid_0005k gst_tripsMadrid_0010k gst_tripsMadrid_0015k gst_tripsMadrid_0020k gst_tripsMadrid_0025k gst_tripsMadrid_0030k gst_tripsMadrid_0035k gst_tripsMadrid_0040k gst_tripsMadrid_0045k gst_tripsMadrid_0050k
 do
-	./TopoRel_GST_5_size ${DATAFOLDER}${filename}.gst5 >> ${OUTPUTFILE}
+	./TopoRel_GST_6_size ${DATAFOLDER}${filename}.gst6 >> ${OUTPUTFILE}
 done
 echo "Fecha FIN experimentos: $(date +'%Y/%m/%d %H:%M:%S')" >> ${OUTPUTFILE}
 exit 0
