6c83487a7beee4a6316e9f43d8f2995cb686e0db
CQF <cquijadafuentes@gmail.com>
2023-05-17 18:41:00 -0400

    update.

--- codes_sdsl_cst/extras/histograma/run_histograma.sh
+++ codes_sdsl_cst/extras/histograma/run_histograma.sh
@@ -10,7 +10,7 @@ DATAFOLDER=$1
 
 g++ -o histograma histograma.cpp
 
-for filename in 20stops_min_05k.txt  20stops_min_10k.txt  20stops_min_15k.txt  20stops_min_20k.txt  20stops_min_25k.txt
+for filename in gst_tripsMadrid_0005k.txt  gst_tripsMadrid_0010k.txt  gst_tripsMadrid_0015k.txt  gst_tripsMadrid_0020k.txt  gst_tripsMadrid_0025k.txt
 do
 	./histograma < ${DATAFOLDER}${filename} > histo_${filename}
 done
