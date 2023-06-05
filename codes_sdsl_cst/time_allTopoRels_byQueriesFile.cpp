a9f37bb412d3f51a569100085bdd9c29ed8eb39f
CQF <cquijadafuentes@gmail.com>
2023-06-05 13:17:53 -0400

    tiempo intersect.

--- codes_sdsl_cst/time_allTopoRels_byQueriesFile.cpp
+++ codes_sdsl_cst/time_allTopoRels_byQueriesFile.cpp
@@ -76,7 +76,7 @@ int main(int argc, char const *argv[]){
 	int resNaive, resGST;
 
 	//////////////////	allContain 	//////////////////
-
+/*
 	resNaive = 0;
 	resGST = 0;
 	cout << "Ejecutando allContain Naive..." << endl;
@@ -102,7 +102,7 @@ int main(int argc, char const *argv[]){
 	
 	cout << "operacion\trutas\tqueries\ttNaive\ttGST\t[us]\trNaive\trGST" << endl;
 	cout << "allContain\t" << gst.n_rutas << "\t" << num_queries << "\t" << tNaive << "\t" << tGST << "\t[us]\t" << resNaive << "\t" << resGST << endl;
-
+*/
 	//////////////////	allContained 	//////////////////
 /*
 	resNaive = 0;
@@ -133,7 +133,7 @@ int main(int argc, char const *argv[]){
 	cout << "allContained\t" << gst.n_rutas << "\t" << num_queries << "\t" << tNaive << "\t" << tGST << "\t[us]\t" << resNaive << "\t" << resGST << endl;
 */
 	//////////////////	allEqual 	//////////////////
-
+/*
 	resNaive = 0;
 	resGST = 0;
 	cout << "Ejecutando allEqual Naive..." << endl;
@@ -160,9 +160,9 @@ int main(int argc, char const *argv[]){
 
 	cout << "operacion\trutas\tqueries\ttNaive\ttGST\t[us]\trNaive\trGST" << endl;
 	cout << "allEqual\t" << gst.n_rutas << "\t" << num_queries << "\t" << tNaive << "\t" << tGST << "\t[us]\t" << resNaive << "\t" << resGST << endl;
-
+*/
 	//////////////////	allIntersect 	//////////////////
-/*
+
 	resNaive = 0;
 	resGST = 0;
 	cout << "Ejecutando allIntersect Naive..." << endl;
@@ -189,7 +189,7 @@ int main(int argc, char const *argv[]){
 
 	cout << "operacion\trutas\tqueries\ttNaive\ttGST\t[us]\trNaive\trGST" << endl;
 	cout << "allIntersect\t" << gst.n_rutas << "\t" << num_queries << "\t" << tNaive << "\t" << tGST << "\t[us]\t" << resNaive << "\t" << resGST << endl;
-*/
+
 	//////////////////	allIntersectPP 	//////////////////
 /*
 	resNaive = 0;
