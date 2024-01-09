2565ab075be9006e0b6bc5794305eb9692fd5678
CQF <cquijadafuentes@gmail.com>
2024-01-09 01:52:28 -0300

    actualización para tomar tiempos en chome.

--- codes_sdsl_cst/run_temporal.sh
+++ codes_sdsl_cst/run_temporal.sh
@@ -13,9 +13,14 @@ echo "Directorio de inicio: "
 pwd
 echo ""
 
-for filename in gst_tripsMadrid_1600k gst_tripsMadrid_3200k
+./extras/dataset_madrid/procesa_madrid_trips_menos_stops ${DATAFOLDER}lineStops.txt ${DATAFOLDER}madrid_trips.txt 1650000
+mv gst_tripsMadrid.txt ${DATAFOLDER}gst_tripsMadrid_1100stops_1600ktrips.txt
+
+for filename in gst_tripsMadrid_1350stops_1600ktrips gst_tripsMadrid_2700stops_1600ktrips gst_tripsMadrid_5500stops_1600ktrips gst_tripsMadrid_1100stops_1600ktrips
 do
 	./TopoRel_GST_build ${DATAFOLDER}${filename}.txt ${DATAFOLDER}${filename}.gst
+	./extras/generador_consultas/generador_consultas ${DATAFOLDER}${filename}.txt ${DATAFOLDER}${filename}.queries.1 5000 1
+	./time_allTopoRels_byQueriesFile ${DATAFOLDER}${filename}.txt ${DATAFOLDER}${filename}.gst ${DATAFOLDER}${filename}.queries.1 1 > ~/resultados_2024-01-09_allTopoRels_stops_variables_1rep.txt &
 done
 
 echo "Fecha FIN construcción: $(date +'%Y/%m/%d %H:%M:%S')"
