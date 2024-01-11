481836b8f473a63834b2b77185738e5671de4c2e
CQF <cquijadafuentes@gmail.com>
2024-01-11 01:56:51 -0300

    update.

--- codes_sdsl_cst/run_temporal.sh
+++ codes_sdsl_cst/run_temporal.sh
@@ -15,10 +15,11 @@ echo "" >> ${OUTPUTFILE}
 
 for filename in gst_800ktrips_02kstops gst_800ktrips_04kstops gst_800ktrips_06kstops gst_800ktrips_08kstops gst_800ktrips_10kstops
 do
+	echo "Procesando ${filename}..." >> ${OUTPUTFILE}
 	./TopoRel_GST_build ${DATAFOLDER}${filename}.txt ${DATAFOLDER}${filename}.gst
 	./TopoRel_GST_size ${DATAFOLDER}${filename}.gst >> ${OUTPUTFILE}
 	./TopoRel_Naive_size ${DATAFOLDER}${filename}.txt >> ${OUTPUTFILE}
-	./time_allTopoRels_byQueriesFile ${DATAFOLDER}${filename}.txt ${DATAFOLDER}${filename}.gst ${DATAFOLDER}${filename}.queries.1 1 >> ${OUTPUTFILE}
+	./time_allTopoRels_byQueriesFile ${DATAFOLDER}${filename}.txt ${DATAFOLDER}${filename}.gst ${DATAFOLDER}${filename}.queries.1 10 >> ${OUTPUTFILE}
 done
 
 echo "Fecha FIN construcción: $(date +'%Y/%m/%d %H:%M:%S')"
