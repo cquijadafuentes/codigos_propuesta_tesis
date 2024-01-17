ea4a241763a1e794b8c940c751969e03c9b0240a
CQF <cquijadafuentes@gmail.com>
2024-01-17 09:48:04 -0300

    update del .run para tamaño de tries.

--- codes_sdsl_cst/naive_tries/run_temporal.sh
+++ codes_sdsl_cst/naive_tries/run_temporal.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "Usage: $(basename $0) <folder> <output_file>"
+if [ $# -ne 3 ]; then
+	echo "Usage: $(basename $0) <folder> <output_file> <exec_to_run>"
 	exit 1
 fi
 
 DATAFOLDER=$1
 OUTPUTFILE=$2
-RUN=suffixPathsTernarySearchTree
+RUN=$3
 
 rm ${RUN}
 g++ -o ${RUN} ${RUN}.cpp
@@ -17,6 +17,7 @@ echo "Directorio de inicio: " >> ${OUTPUTFILE}
 pwd >> ${OUTPUTFILE}
 echo "Directorio de datos: ${DATAFOLDER}" >> ${OUTPUTFILE}
 echo "" >> ${OUTPUTFILE}
+echo "Corriendo ${RUN}" >> ${OUTPUTFILE}
 
 for filename in gst_tripsMadrid_0005k.txt gst_tripsMadrid_0010k.txt gst_tripsMadrid_0015k.txt gst_tripsMadrid_0020k.txt gst_tripsMadrid_0025k.txt gst_tripsMadrid_0030k.txt gst_tripsMadrid_0035k.txt gst_tripsMadrid_0040k.txt gst_tripsMadrid_0045k.txt gst_tripsMadrid_0050k.txt gst_tripsMadrid_0100k.txt gst_tripsMadrid_0150k.txt gst_tripsMadrid_0200k.txt gst_tripsMadrid_0400k.txt gst_tripsMadrid_0800k.txt gst_tripsMadrid_1600k.txt gst_tripsMadrid_3200k.txt gst_tripsMadrid_6400k.txt
 do
@@ -24,6 +25,6 @@ do
 	./${RUN} ${DATAFOLDER}${filename} >> ${OUTPUTFILE}
 done
 
-echo "Fecha FIN: $(date +'%Y/%m/%d %H:%M:%S')"
+echo "Fecha FIN: $(date +'%Y/%m/%d %H:%M:%S')" >> ${OUTPUTFILE}
 exit 0
 
