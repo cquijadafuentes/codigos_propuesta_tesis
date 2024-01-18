6b098e0678ec0ec7ed82cda7ca416cfc3e7810a8
CQF <cquijadafuentes@gmail.com>
2024-01-18 01:05:03 -0300

    update.

--- codes_sdsl_cst/naive_tries/run_temporal.sh
+++ codes_sdsl_cst/naive_tries/run_temporal.sh
@@ -19,7 +19,7 @@ echo "Directorio de datos: ${DATAFOLDER}" >> ${OUTPUTFILE}
 echo "" >> ${OUTPUTFILE}
 echo "Corriendo ${RUN}" >> ${OUTPUTFILE}
 
-for filename in gst_tripsMadrid_0005k.txt gst_tripsMadrid_0010k.txt gst_tripsMadrid_0015k.txt gst_tripsMadrid_0020k.txt gst_tripsMadrid_0025k.txt gst_tripsMadrid_0030k.txt gst_tripsMadrid_0035k.txt gst_tripsMadrid_0040k.txt gst_tripsMadrid_0045k.txt gst_tripsMadrid_0050k.txt gst_tripsMadrid_0100k.txt gst_tripsMadrid_0150k.txt gst_tripsMadrid_0200k.txt gst_tripsMadrid_0400k.txt gst_tripsMadrid_0800k.txt gst_tripsMadrid_1600k.txt gst_tripsMadrid_3200k.txt gst_tripsMadrid_6400k.txt
+for filename in gst_tripsMadrid_0005k.txt gst_tripsMadrid_0010k.txt gst_tripsMadrid_0015k.txt gst_tripsMadrid_0020k.txt gst_tripsMadrid_0025k.txt gst_tripsMadrid_0030k.txt gst_tripsMadrid_0035k.txt gst_tripsMadrid_0040k.txt gst_tripsMadrid_0045k.txt gst_tripsMadrid_0050k.txt gst_tripsMadrid_0100k.txt gst_tripsMadrid_0200k.txt gst_tripsMadrid_0400k.txt gst_tripsMadrid_0800k.txt gst_tripsMadrid_1600k.txt gst_tripsMadrid_3200k.txt gst_tripsMadrid_6400k.txt
 do
 	echo "Procesando ${filename}..." >> ${OUTPUTFILE}
 	./${RUN} ${DATAFOLDER}${filename} >> ${OUTPUTFILE}
