abea860c45eac6d802b3a56c1d467f6ad5d86f5c
CQF <cquijadafuentes@gmail.com>
2024-01-19 01:45:17 -0300

    update

---
+++ codes_sdsl_cst/naive_tries/suffixArrayEstimation.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	if(argc < 2){
+		cout << "Error! Faltan argumentos." << endl;
+		cout << argv[0] << " <input_file>" << endl;
+		return 0;
+	}
+	// Cargando datos
+	ifstream entrada(argv[1], ifstream::in);
+	int n, x, aux, max;
+	entrada >> n >> max;
+	int cantSeqConcat = 0;
+	for(int i = 0; i < n; i++){
+		entrada >> x;
+		for(int j=0; j<x; j++){
+			entrada >> aux;
+		}
+		cantSeqConcat += (x+1); 	// La secuencia completa + separador entre secuencias
+	}
+	cantSeqConcat += 1; 		// El caracter de fin de secuencia (0)
+	entrada.close();
+	double logaritmo = log2(cantSeqConcat);
+	double bytes = ceil(logaritmo)/8;
+	cout << "rutas\tsecConcat\tbytes" << endl;
+	cout << n << "\t" << cantSeqConcat << "\t" << bytes << endl;
+	return 0; 
+}
